cba3e7191a4e4cf18491dd57e248dd533ce5a764
SamuelPS0 <rm93980@estudante.fieb.edu.br>
2025-06-05 20:02:52 +0000

    tentando-arrumar-dropdowns

--- my-project-TCC/src/App.jsx
+++ my-project-TCC/src/App.jsx
@@ -9,22 +9,18 @@ import Header from './Components/Header';
 import Category from './Components/Category';
 import SearchBar from './Components/SearchBar';
 
-function App() {
+export default function App() {
   return (
     <>
-    <SearchBar />
-       <Header />
-       <div className='categorias'>
-       <Category texto="Modelo de trabalho"/>
-       <Category texto="Publicado em"/>
-       <Category texto="Distância"/>
-       <Category texto="Preço"/>
-       <Category texto="Categorias"/>
-       <Category texto="Disponibilidade"/>
-       </div>
-       
+      <Header />
+      <div className="categorias">
+        <Category texto="Modelo de trabalho" />
+        <Category texto="Publicado em" />
+        <Category texto="Distância" />
+        <Category texto="Preço" />
+        <Category texto="Categorias" />
+        <Category texto="Disponibilidade" />
+      </div>
     </>
   );
 }
-
-export default App
--- my-project-TCC/src/Components/Header.jsx
+++ my-project-TCC/src/Components/Header.jsx
@@ -1,29 +1,23 @@
+// Header.jsx
 import React from 'react';
-
 import './Header.css';
 
 export default function Header() {
   return (
-
-    <div>
-        <header className='header'>
-            <a href="/" className='Logo'></a>
-          
-
-    <nav className='nav'>
+    <header className="header">
+      <a href="/" className="logo" aria-label="Página inicial" />
+      <nav className="nav">
         <a href="#">Sobre nós</a>
         <a href="#">Encontrar serviços</a>
         <a href="#">Mais procurados</a>
         <a href="#">Categorias</a>
-        <span className='frame-2'>
-        <a href="#">Empresas perto de mim</a>
-        <span className='container-login'>
-        <a href="#">Login</a>
-        </span>
-        </span>
-    </nav>
-   </header>
-    </div>
-        
+        <div className="frame-2">
+          <a href="#">Empresas perto de mim</a>
+          <span className="container-login">
+            <a href="#">Login</a>
+          </span>
+        </div>
+      </nav>
+    </header>
   );
 }
