75e4fc80e8c5b2bc4859816ec1ab5dcb24361bb8
SamuelPS0 <rm93980@estudante.fieb.edu.br>
2025-06-11 10:47:12 +0000

    SideMenu links atribuidos

--- my-project-TCC/src/Components/SideMenu/SideMenu.jsx
+++ my-project-TCC/src/Components/SideMenu/SideMenu.jsx
@@ -3,15 +3,17 @@ import { IoPersonCircle } from "react-icons/io5";
 import { IoShieldOutline } from "react-icons/io5";
 import { IoMdArrowBack } from "react-icons/io";
 import { CiLogout } from "react-icons/ci";
+import { Link } from 'react-router-dom';
 import './SideMenu.css'
 
 export default function SideMenu(){
     return(
         <div className='container-sidemenu'>
             <ul>
-                <li className='menu-li'><IoPersonCircle />Perfil</li>
-                <li className='menu-li'><IoShieldOutline />Informações da conta</li>
-                <li className='menu-li'><IoMdArrowBack />Voltar a tela inicial</li>
+                
+                <li><Link to="/create-perfil" className='menu-li'><IoPersonCircle />Perfil</Link></li>
+                <li><Link to='/acc-info' className='menu-li'><IoShieldOutline />Informações da conta</Link></li>
+                <li><Link to='/' className='menu-li'><IoMdArrowBack />Voltar a tela inicial</Link></li>
                 <li className='menu-li'><CiLogout />Sair</li>
             </ul>
         </div>
